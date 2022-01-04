d069994bf75f5f6b5c7b1fbf59f8708e62fc8afa
1375272391 <1375272391@qq.com>
2022-01-04 19:20:17 +0800

    first

---
+++ omni_m18s.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := m18s
+
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit device configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := m18s
+PRODUCT_NAME := omni_m18s
+PRODUCT_BRAND := Coolpad
+PRODUCT_MODEL := Coolpad 5270
+PRODUCT_MANUFACTURER := Coolpad
+
